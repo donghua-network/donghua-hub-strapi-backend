68b892df2f27fffc42e7b94af940c11daceb3ff1
Oliver Fei <ofei006@uottawa.ca>
2021-04-17 22:12:54 -0400

    Update donghua api response format

--- api/donghua/controllers/donghua.js
+++ api/donghua/controllers/donghua.js
@@ -1,8 +1,46 @@
-'use strict';
+const { sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const replaceImageWithUrl = (donghua) => {
+  if (donghua.image && donghua.image.url) {
+    donghua.image = donghua.image.url;
+  }
+  return donghua;
+};
 
-module.exports = {};
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
+
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.donghua.search(ctx.query);
+    } else {
+      entities = await strapi.services.donghua.find(ctx.query);
+    }
+
+    return entities.map((entity) =>
+      replaceImageWithUrl(
+        sanitizeEntity(entity, { model: strapi.models.donghua })
+      )
+    );
+  },
+
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.donghua.findOne({ id });
+    return replaceImageWithUrl(
+      sanitizeEntity(entity, { model: strapi.models.donghua })
+    );
+  },
+};
