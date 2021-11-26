2d32f8dd035d23725e2dd861bfefce36e99f1faa
Oliver Fei <ofei006@uottawa.ca>
2021-11-26 03:56:45 -0500

    Add custom image file field to donghua

--- api/donghua/controllers/donghua.js
+++ api/donghua/controllers/donghua.js
@@ -1,8 +1,47 @@
-"use strict";
+const { sanitizeEntity } = require("strapi-utils");
+// TODO get rid of this hardcoded variable
+const PROD_FILE_PREFIX = "https://files.donghuahub.com/";
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.donghua.search(ctx.query);
+    } else {
+      entities = await strapi.services.donghua.find(ctx.query);
+    }
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+    for (const entity of entities) {
+      if (entity.image) {
+        if (strapi.config.currentEnvironment == "production") {
+          const urlParts = entity.image.url.split("/");
+          entity.imageUrl = PROD_FILE_PREFIX + urlParts[urlParts.length - 1];
+        } else {
+          entity.imageUrl = entity.image.url;
+        }
+      } else {
+        entity.imageUrl = null;
+      }
+    }
 
-module.exports = {};
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.donghua })
+    );
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.donghua.findOne({ id });
+    if (entity.image) {
+      if (strapi.config.currentEnvironment == "production") {
+        const urlParts = entity.image.url.split("/");
+        entity.imageUrl = PROD_FILE_PREFIX + urlParts[urlParts.length - 1];
+      } else {
+        entity.imageUrl = entity.image.url;
+      }
+    } else {
+      entity.imageUrl = null;
+    }
+    return sanitizeEntity(entity, { model: strapi.models.donghua });
+  },
+};
